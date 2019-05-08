2b40ed9c3e936746f79a36b455edac575aa69109
wilder Delgado <willy8086@gmail.com>
2019-05-07 23:49:42 -0300

    first commit

---
+++ src/main/java/ar/edu/utn/frba/tadp/JposgatewayspringbootApplication.java
@@ -0,0 +1,13 @@
+package ar.edu.utn.frba.tadp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JposgatewayspringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JposgatewayspringbootApplication.class, args);
+	}
+
+}
